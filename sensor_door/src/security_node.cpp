21372f8ac67ff7b96b0fffd5c55b2479c1620977
Rafael Martin <theverbat@gmail.com>
2022-03-22 19:07:04 +0100

    Implemented msg in node

--- sensor_door/src/security_node.cpp
+++ sensor_door/src/security_node.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 
 #include <sensor_door_msgs/MultipleDoorStatus.h>
+#include <sensor_door_msgs/DoorSecurity.h>
 #include <std_srvs/Empty.h>
 #include <kobuki_msgs/Sound.h>
 
@@ -8,50 +9,46 @@ struct CheckerDoors
 {
     ros::Subscriber door_status;
     ros::ServiceServer rearm_srv;
-    ros::Publisher sound;
-    ros::Timer remember_sound;
+    ros::Publisher system_pub;
 
     bool is_secure {false};
 
     CheckerDoors(ros::NodeHandle& nh)
       : door_status {nh.subscribe("door_status", 10, &CheckerDoors::callback_door_status, this)}
       , rearm_srv {nh.advertiseService("rearm", &CheckerDoors::callback_rearm, this) }
-      , sound { nh.advertise<kobuki_msgs::Sound>("sound", 10, false) }
-      , remember_sound { nh.createTimer(ros::Duration(7), &CheckerDoors::callback_remember_sound, this, false, false) }
+      , system_pub { nh.advertise<sensor_door_msgs::DoorSecurity>("sys_status", 10, false) }
     {}
 
     void callback_door_status(const sensor_door_msgs::MultipleDoorStatusConstPtr msg)
     {
         const std::size_t n_door = msg->doors.size();
+        sensor_door_msgs::DoorSecurity s;
+        
+
         for (std::size_t i {0} ; i<n_door ; ++i)
         {
             const sensor_door_msgs::DoorStatus& cur_door = msg->doors[i];
             if (cur_door.connected && cur_door.last_response_successfull && cur_door.door_status)
                 continue;
+            
+            ROS_INFO_COND(is_secure, "System has detected intrusion. Disarmed!");
             is_secure = false;
-            if (cur_door.connected && cur_door.last_response_successfull && !cur_door.door_status)
+            
+            if (!cur_door.connected)
             {
-                kobuki_msgs::Sound s;
-                s.value = kobuki_msgs::Sound::CLEANINGSTART;
-                sound.publish(s);
+                ROS_WARN_DELAYED_THROTTLE(10, "Disconnected devices is added.");
             }
         }
-        ROS_INFO("xD %d", static_cast<int>(is_secure));
+        s.header = msg->header;
+        s.is_secure = is_secure;
+        system_pub.publish(s);
     }
 
     bool callback_rearm(std_srvs::EmptyRequest& req, std_srvs::EmptyResponse& res)
     {
-        ROS_INFO("Rearming sensor door");
+        ROS_INFO_COND(!is_secure, "Service to rearm called. Reaming!");
         is_secure = true;
-        remember_sound.stop();
         return true;
-    }
-
-    void callback_remember_sound(const ros::TimerEvent& t)
-    {
-        kobuki_msgs::Sound s;
-        s.value = kobuki_msgs::Sound::ERROR;
-        sound.publish(s);
     }
 };
 
